cec147e83da501db30907085c2f8870ee8977ef1
Magali Fabri <magali_90@live.nl>
2022-04-04 20:23:34 +0200

    First commit: state: ...
    - laravel project
    - composer packages: breeze, ide-helper, clockwork
    - npm install
    - basic homepage infrastructure

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <h1>header</h1>
+
+    @auth
+        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+    @else
+        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+        @if (Route::has('register'))
+            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+        @endif
+    @endauth
+</header>
+
+<hr>
+
+<main>
+    <p>main</p>
+</main>
+
+<hr>
+
+<footer>
+    <p>footer</p>
+</footer>
+</body>
+</html>
