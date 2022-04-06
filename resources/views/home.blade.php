d5524815f4851cc0e3de31147e7e7cf6e17538e3
Magali Fabri <magali_90@live.nl>
2022-04-06 09:50:32 +0200

    Use my_app layout blade for home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<header>
-    <h1>header</h1>
+@extends('layouts.my_app')
 
-    @auth
-        <a href="{{ url('/dashboard') }}">Dashboard</a>
-    @else
-        <a href="{{ route('login') }}">log in</a>
-        <span>|</span>
-        <a href="{{ route('register') }}">register</a>
-    @endauth
-</header>
+@section('main')
 
-<hr>
-
-<main>
     <p>main</p>
-</main>
 
-<hr>
-
-<footer>
-    <p>footer</p>
-</footer>
-</body>
-</html>
+@endsection
