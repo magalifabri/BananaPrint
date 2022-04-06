5cb2b6c7d75fa64dea07ad1b92a223c0e65838cc
Magali Fabri <magali_90@live.nl>
2022-04-06 11:48:20 +0200

    Display printer info on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    {{--                    You're logged in!--}}
 
-                    <p><a href="{{ route('createPrinter') }}">Add Printer</a></p>
+                    @unless ($user['has_printer'])
+                        <p><a href="{{ route('createPrinter') }}">Add Printer</a></p>
+                    @else
+                        <h2>your printer</h2>
+                        <p>
+                            {{ $user->printer['street'] }}
+                            {{ $user->printer['street_number'] }},
+                            {{ $user->printer['zipcode'] }}
+
+                        </p>
+                    @endif
                 </div>
             </div>
         </div>
