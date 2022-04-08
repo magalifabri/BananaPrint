497345b80c443d0dd02343a0f4f870d1d978f674
Magali Fabri <magali_90@live.nl>
2022-04-08 22:05:33 +0200

    Add intro section to home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,14 @@
 <x-layout>
 
+    <section class="intro">
+        <img src="{{ asset('images/bananas.jpg') }}"
+             alt="image of a woman's hand holding a bunch of banana on a yellow background">
+
+        <div class="text-container">
+            <h2>Connecting those without printers to those with, for little print jobs.</h2>
+            <p>As reciprocation, we suggest a banana. By many claimed to be the ultimate healthy snack!</p>
+        </div>
+    </section>
     <p>
         search for printers near <input class="search-input-field" type="text" name="location"
                                         placeholder="street, city / zipcode">
