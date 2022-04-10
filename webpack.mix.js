917bc3d2b59515061684107dd78ef60becd23d37
Magali Fabri <magali_90@live.nl>
2022-04-10 12:37:08 +0200

    Add BrowserSync package

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix
+    .browserSync('http://localhost:8000/')
+    .js('resources/js/app.js', 'public/js')
     .js('resources/js/index.js', 'public/js')
     .js('resources/js/map.js', 'public/js')
     .sass('resources/sass/style.scss', 'public/css')
