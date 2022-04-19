834ffd4f54189aafaac6d60ac3d7b358db30412a
Magali Fabri <magali_90@live.nl>
2022-04-19 10:27:58 +0200

    Set db parameters to connect to heroku db

--- config/database.php
+++ config/database.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Str;
 
+//$DATABASE_URL=parse_url('Your database URL');
+$dbParams = parse_url(getenv("DATABASE_URL"));
+
 return [
 
     /*
@@ -66,11 +69,17 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $dbParams["host"],
+            'port' => $dbParams["port"],
+            'database' => ltrim($dbParams["path"], "/"),
+            'username' => $dbParams["user"],
+            'password' => $dbParams["pass"],
+
+//            'host' => env('DB_HOST', '127.0.0.1'),
+//            'port' => env('DB_PORT', '5432'),
+//            'database' => env('DB_DATABASE', 'forge'),
+//            'username' => env('DB_USERNAME', 'forge'),
+//            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
